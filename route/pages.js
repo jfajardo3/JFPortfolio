c5170d0a9c1c19ac696d6eef3e5d1c2feacb8b8a
jfajardo3 <89419276+jfajardo3@users.noreply.github.com>
2022-08-03 09:51:51 -0500

    contactFormMethod

---
+++ route/pages.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const Contact = require('./models/contact')
+
+router.get('/', (req, res) => {
+    res.render('index')
+})
+
+router.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+router.get('/', (req, res) => {
+    res.render('education')
+})
+
+router.get('/experience', (req, res) => {
+    res.render('experience')
+})
+
+router.get('/graphicDesign', (req, res) => {
+    res.render('graphicDesign')
+})
+
+router.get('/photography', (req, res) => {
+    res.render('photography')
+})
+
+router.post('/submitContact', (req, res) => {
+    const contact = new Contact ({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        comment: req.body.comment
+    });
+
+    Contact.collection.insertOne(contact)
+    .then(result => {
+        res.render('contact')
+    })
+    .catch(err => console.log(err));
+})
+
+module.exports = router;
